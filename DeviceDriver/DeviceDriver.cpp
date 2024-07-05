1cf1b31b2156b4508a55d592eae767b3c8d7841d
j1.woo <j1.woo@samsung.com>
2024-07-05 09:49:50 +0900

    develop device driver

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -7,11 +7,22 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int firstRdata = (int)(m_hardware->read(address));
+
+    for (int i = 0; i < NR_READ_ITERATION-1; i++)
+    {
+        if (firstRdata != (int)(m_hardware->read(address)))
+            throw std::exception("ReadFailException");
+    }
+
+    return firstRdata;
 }
 
 void DeviceDriver::write(long address, int data)
 {
     // TODO: implement this method
+    if((int)(m_hardware->read(address)) != 0xFF)
+        throw std::exception("WriteFailException");
+
     m_hardware->write(address, (unsigned char)data);
 }
