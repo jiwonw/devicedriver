6ccd8498df5464c3dca3a506fcb0230eddf8916d
j1.woo <j1.woo@samsung.com>
2024-07-05 11:25:18 +0900

    application

--- DeviceDriver_test/test.cpp
+++ DeviceDriver_test/test.cpp
@@ -2,6 +2,8 @@
 #include "gmock/gmock.h"
 #include "../DeviceDriver/DeviceDriver.cpp"
 #include "TestableFlash.cpp"
+#include "../DeviceDriver/Application.cpp"
+#include "TestableDeviceDriver.cpp"
 
 class DeviceDriverFixture : public testing::Test {
 protected:
@@ -9,16 +11,18 @@ protected:
 	}
 
 public:
-	TestableFlash TEST_FLASH;
+
+	testing::NiceMock<TestableFlash> TEST_FLASH;
 	DeviceDriver dd{&TEST_FLASH};
 };
 
-TEST_F(DeviceDriverFixture, 읽기_5번같은데이터) {
 
-	EXPECT_CALL(TEST_FLASH, read, (long address), ())
+TEST_F(DeviceDriverFixture, 읽기_5번같은데이터) {
+	long addr = 0x4321;
+	EXPECT_CALL(TEST_FLASH, read(addr), ())
 		.Times(5);
 
-	dd.read(0);
+	dd.read(addr);
 }
 
 TEST_F(DeviceDriverFixture, 읽기_5번같은데이터아님) {
@@ -41,32 +45,133 @@ TEST_F(DeviceDriverFixture, 읽기_5번같은데이터아님) {
 
 TEST_F(DeviceDriverFixture, 쓰기_읽고쓰기) {
 	// Arrange
-	EXPECT_CALL(TEST_FLASH, read, (long address), ())
+	long addr = 0x55;
+	int data = 0x1234;
+	EXPECT_CALL(TEST_FLASH, read(addr))
 		.Times(1)
 		.WillOnce(testing::Return(0xff));
-	EXPECT_CALL(TEST_FLASH, write, (long address, unsigned char data), ())
+	EXPECT_CALL(TEST_FLASH, write(addr, data), ())
 		.Times(1);
 
 	// Act
-	dd.write(0, 0x1234);
+	dd.write(addr, data);
 }
 
 TEST_F(DeviceDriverFixture, 쓰기_이미쓰인영역) {
 	// Arrange
-	EXPECT_CALL(TEST_FLASH, read, (long address), ())
+	long addr = 0x55;
+	int data = 0x1234;
+	EXPECT_CALL(TEST_FLASH, read(addr), ())
 		.Times(1)
 		.WillOnce(testing::Return(0x1234));
-	EXPECT_CALL(TEST_FLASH, write, (long address, unsigned char data), ())
+	EXPECT_CALL(TEST_FLASH, write(addr, data), ())
 		.Times(0);
 
 	// Act
 	// EXPECT_THROW(dd.write(0, 0x1234), exception, "WriteFailException");
 	try {
-		dd.write(0, 0x1234);
+		dd.write(addr, data);
 	}
 	catch (exception& e)
 	{
 		// Assert
 		EXPECT_EQ(string("WriteFailException"), e.what());
 	}
+}
+
+// ===========================
+
+class ApplicationFixture : public testing::Test {
+protected:
+	void SetUp() override {
+	}
+
+public:
+	testing::NiceMock<TestableFlash> TEST_FLASH;
+	DeviceDriver dd{ &TEST_FLASH };
+	Application App{ &dd };
+};
+
+TEST_F(ApplicationFixture, App_범위읽기) {
+	// Arrange
+	long startAddr = 0;
+	long endAddr = 7;
+	long nr_read = endAddr - startAddr + 1;
+
+	EXPECT_CALL(TEST_FLASH, read)
+		.Times(nr_read*5)
+		.WillOnce(testing::Return(3))
+		.WillOnce(testing::Return(3))
+		.WillOnce(testing::Return(3))
+		.WillOnce(testing::Return(3))
+		.WillOnce(testing::Return(3))
+		.WillOnce(testing::Return(2))
+		.WillOnce(testing::Return(2))
+		.WillOnce(testing::Return(2))
+		.WillOnce(testing::Return(2))
+		.WillOnce(testing::Return(2))
+		.WillOnce(testing::Return(1))
+		.WillOnce(testing::Return(1))
+		.WillOnce(testing::Return(1))
+		.WillOnce(testing::Return(1))
+		.WillOnce(testing::Return(1))
+		.WillOnce(testing::Return(0))
+		.WillOnce(testing::Return(0))
+		.WillOnce(testing::Return(0))
+		.WillOnce(testing::Return(0))
+		.WillOnce(testing::Return(0))
+		.WillRepeatedly(testing::Return(0xf));
+
+	App.ReadAndPrint(startAddr, endAddr);
+}
+
+TEST_F(ApplicationFixture, App_범위쓰기) {
+	// Arrange
+	int nr_read = 5;
+	int data = 0x1234;
+
+	EXPECT_CALL(TEST_FLASH, read)
+		.Times(nr_read)
+		.WillRepeatedly(testing::Return(0xff));
+
+	EXPECT_CALL(TEST_FLASH, write, (_,data), ())
+		.Times(nr_read);
+
+	App.WriteAll(data);
+}
+
+// ===========================
+
+class ApplicationFixture2 : public testing::Test {
+protected:
+	void SetUp() override {
+	}
+
+public:
+	TestableDeviceDriver TEST_DEVICE_DRIVER;
+	Application App{ &TEST_DEVICE_DRIVER };
+};
+
+
+TEST_F(ApplicationFixture2, App_범위읽기) {
+	// Arrange
+	long startAddr = 0;
+	long endAddr = 7;
+	long nr_read = endAddr - startAddr + 1;
+
+	EXPECT_CALL(TEST_DEVICE_DRIVER, read)
+		.Times(nr_read);
+
+	App.ReadAndPrint(startAddr, endAddr);
+}
+
+TEST_F(ApplicationFixture2, App_범위쓰기) {
+	// Arrange
+	int nr_read = 5;
+	int data = 0x1234;
+
+	EXPECT_CALL(TEST_DEVICE_DRIVER, write, (_, data), ())
+		.Times(nr_read);
+
+	App.WriteAll(data);
 }
